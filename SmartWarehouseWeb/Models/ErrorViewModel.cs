2705c6a9697b2c10aa2c19e2b117196981a08c76
Doan Hai <honghai3501@gmail.com>
2021-06-06 20:28:55 +0700

    ccc

---
+++ SmartWarehouseWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartWarehouseWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
